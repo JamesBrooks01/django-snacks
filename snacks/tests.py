c493da280cb061e5f2c389dd920004ec437d6abe
James Brooks <brooksjamesk01@gmail.com>
2022-05-23 18:23:47 -0700

    added a few tests for the status codes and home page template

---
+++ snacks/tests.py
@@ -0,0 +1,19 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+class SnacksTest(SimpleTestCase):
+  def test_home_page_status(self):
+    url = reverse('home')
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 200)
+  
+  def test_about_page_status(self):
+    url = reverse('about')
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 200)
+  
+  def test_home_page_template(self):
+    url = reverse('home')
+    response = self.client.get(url)
+    self.assertTemplateUsed(response, 'home.html')
+    self.assertTemplateUsed(response, 'base.html')
